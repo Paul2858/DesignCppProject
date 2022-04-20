95c347aef7fe929102c5c6c104d81ed412075b18
Paul2858 <pc2858@columbia.edu>
2022-04-19 22:19:26 -0400

    comments

--- PathFinder.cpp
+++ PathFinder.cpp
@@ -7,6 +7,7 @@ class PathFinder{
 
 	GeneralPath findShortestPath(){
 // returns shortest path of a GeneralMapObject given start and end
-
+// Runs Dijkstra if GeneralMapObject is graph
+// Runs backtracking algorithm if maze/matrix
 	}
 }
