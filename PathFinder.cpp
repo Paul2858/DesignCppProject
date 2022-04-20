03436306baa5e9118f9b332eac3bf4328a0e40ab
Paul2858 <pc2858@columbia.edu>
2022-04-19 22:14:13 -0400

    skel

---
+++ PathFinder.cpp
@@ -0,0 +1,12 @@
+class PathFinder{
+
+	GeneralMapObject createPathFinder(){
+// takes in as input the "map", start, finish and returns relevant object
+
+	}	
+
+	GeneralPath findShortestPath(){
+// returns shortest path of a GeneralMapObject given start and end
+
+	}
+}
