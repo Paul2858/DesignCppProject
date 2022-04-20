097e0cc282cfa60c7fa7381f0523dc62222a5700
Paul2858 <pc2858@columbia.edu>
2022-04-20 11:58:42 -0400

    ..?

--- Graph.cpp
+++ Graph.cpp
@@ -1,5 +1,18 @@
+
+
+template <class NodeType, class WeightType>
+
+struct Edge{
+	NodeType start;
+	NodeType end;
+	WeightType weight;
+
+}
+
 class Graph{
 
+	// Function that adds Edge structs to list
+	// Function that prints graph
 
 
 }
