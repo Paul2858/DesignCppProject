a2d659eba8bd365e8276513a7f378e71318458cd
OwlPudding <acortesose@gmail.com>
2022-04-28 13:48:38 -0400

    make basic example work again

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
-#include "Graph.h"
-#include "PathFinder.h"
+#include "APathy.h"
+
+struct Edge {
+	int src, dest, weight;
+};
 
 int main() {
   std::vector<Edge> edges = {
     // (x, y, w) —> edge from `x` to `y` having weight `w`
     {0, 1, 6}, {1, 2, 7}, {2, 0, 5}, {2, 1, 4}, {3, 2, 10}, {5, 4, 1}, {4, 5, 3}
   };
-  // int s = (int) edges.size();
-  Graph graph(edges);
+  APathy<Edge, int, int> finder(edges, 6);
 
-  std::vector<int> sp = PathFinder::AStar(graph, 3, 1);
+  std::vector<int> sp = finder.AStar(3, 1);
   std::cout << "Found path: [ ";
   for (int i : sp)
     std::cout << i << ' ';
